291923d8e2c614e81d39e0d242b687ea8cd0770d
penglei <penglei@ybyte.org>
2023-03-14 12:45:52 +0800

    enable vim-go; disable t mode keymaps

--- lua/modules/plugins/lang.lua
+++ lua/modules/plugins/lang.lua
@@ -1,11 +1,11 @@
 local lang = {}
 
--- lang["fatih/vim-go"] = {
--- 	lazy = true,
--- 	ft = "go",
--- 	build = ":GoInstallBinaries",
--- 	config = require("lang.vim-go"),
--- }
+lang["fatih/vim-go"] = {
+	lazy = true,
+	ft = "go",
+	build = ":GoInstallBinaries",
+	config = require("lang.vim-go"),
+}
 lang["simrat39/rust-tools.nvim"] = {
 	lazy = true,
 	ft = "rust",
