eb1342b2d8f41f8e4b46bb9a24c333fead4e5556
penglei <penglei@ybyte.org>
2024-07-01 15:02:48 +0800

    add terraform commentstring with lua api

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -73,6 +73,14 @@ vim.api.nvim_create_autocmd("BufRead", {
   end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+  callback = function(ev)
+    vim.bo[ev.buf].commentstring = "# %s"
+  end,
+  pattern = { "terraform", "hcl" },
+})
+
 function autocmd.load_autocmds()
   local definitions = {
     lazy = {},
