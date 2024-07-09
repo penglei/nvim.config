a1f2e6b4875180d546bb93658d0ca530f879c0ca
penglei <penglei@ybyte.org>
2024-07-09 22:02:16 +0800

    format treesitter config

--- lua/modules/configs/editor/treesitter.lua
+++ lua/modules/configs/editor/treesitter.lua
@@ -1,83 +1,80 @@
 return vim.schedule_wrap(function()
-	vim.api.nvim_set_option_value("foldmethod", "expr", {})
-	vim.api.nvim_set_option_value("foldexpr", "nvim_treesitter#foldexpr()", {})
+  vim.api.nvim_set_option_value("foldmethod", "expr", {})
+  vim.api.nvim_set_option_value("foldexpr", "nvim_treesitter#foldexpr()", {})
 
-	require("nvim-treesitter.configs").setup({
-		ensure_installed = {
-			"bash",
-			"c",
-			"cpp",
-			"css",
-			"go",
-			"gomod",
-			"html",
-			"javascript",
-			"json",
-			"latex",
-			"lua",
-			"make",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"rust",
-			"typescript",
-			"yaml",
-			"nickel",
-			"nix",
-			"scheme",
-			"elvish",
-			"vimdoc",
-		},
-		highlight = {
-			enable = true,
-			disable = function(ft, bufnr)
-				-- vim.notify(string.format("determine whether disable tree-sitter highlight for type: %s", ft))
-				if vim.tbl_contains({ "vim" }, ft) then
-					return true
-				end
+  require("nvim-treesitter.configs").setup({
+    ensure_installed = {
+      "bash",
+      "c",
+      "cpp",
+      "css",
+      "go",
+      "gomod",
+      "html",
+      "javascript",
+      "json",
+      "latex",
+      "lua",
+      "make",
+      -- "markdown",
+      -- "markdown_inline",
+      "python",
+      "rust",
+      "typescript",
+      "yaml",
+      "nickel",
+      "nix",
+      "scheme",
+      "elvish",
+      "vimdoc",
+    },
 
-				local ok, is_large_file = pcall(vim.api.nvim_buf_get_var, bufnr, "bigfile_disable_treesitter")
-				return ok and is_large_file
-			end,
-			additional_vim_regex_highlighting = { "c", "cpp" },
-		},
-		textobjects = {
-			select = {
-				enable = true,
-				keymaps = {
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]["] = "@function.outer",
-					["]m"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]]"] = "@function.outer",
-					["]M"] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[["] = "@function.outer",
-					["[m"] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[]"] = "@function.outer",
-					["[M"] = "@class.outer",
-				},
-			},
-		},
-		rainbow = {
-			enable = true,
-			extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-			max_file_lines = 2000, -- Do not enable for files with more than 2000 lines, int
-		},
-		matchup = { enable = true },
-	})
-	require("nvim-treesitter.install").prefer_git = true
+    highlight = {
+      enable = true,
+      disable = function(ft, bufnr)
+        if vim.tbl_contains({ "gitcommit" }, ft) or (vim.api.nvim_buf_line_count(bufnr) > 7500 and ft ~= "vimdoc") then
+          return true
+        end
+
+        local ok, is_large_file = pcall(vim.api.nvim_buf_get_var, bufnr, "bigfile_disable_treesitter")
+        return ok and is_large_file
+      end,
+      additional_vim_regex_highlighting = false,
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["]["] = "@function.outer",
+          ["]m"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]]"] = "@function.outer",
+          ["]M"] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[["] = "@function.outer",
+          ["[m"] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[]"] = "@function.outer",
+          ["[M"] = "@class.outer",
+        },
+      },
+    },
+    indent = { enable = true },
+    matchup = { enable = true },
+  })
+  require("nvim-treesitter.install").prefer_git = true
 end)
